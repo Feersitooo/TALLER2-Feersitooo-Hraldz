1987a564a95c0c37901cb030b3950e9cf0fe3259
Zambra <joanzambras@gmail.com>
2026-05-06 09:32:22 -0400

    logica en main, limpieza de clases.

--- MainyyClases/Clases/Gimnasio.java
+++ MainyyClases/Clases/Gimnasio.java
@@ -1,5 +1,36 @@
 package Taller;
 
-public class Gimnasio {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Gimnasio {
+	
+	
+	
+	public static void mostrarGimnasios(String ruta) {
+		//mostrar los gimnasios
+		try (BufferedReader lector = new BufferedReader(new FileReader(ruta))){
+			String linea;
+			
+			while((linea = lector.readLine()) != null) {
+				
+			}
+			
+
+		}catch(IOException e) {
+			
+		}
+	}
+
+	
+	// editar estado del gimnasio
+	
+	
+	
+	
+	
+	
+	
+	
 }
